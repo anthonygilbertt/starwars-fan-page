452b7227fa4e7d1511b61c593c860c88ce223d2a
Anthony <anthony.gilbert93@gmail.com>
2021-03-24 03:25:49 -0500

    Added Initial UI

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
     <Nuxt />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.4/tailwind.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
+    />
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -58,5 +63,290 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+* {
+  box-sizing: border-box;
+}
+body {
+  background-image: url("https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F04%2FStars-wallpaper-HD-desktop.jpg&f=1&nofb=1");
+  color: #08e8de !important;
+}
+
+.page-wrap {
+  width: 800px;
+  margin: 20px auto;
+  border-color: #08e8de;
+  border-width: 2px;
+}
+
+.main-story {
+  position: relative;
+  margin: 0 0 25px 0;
+}
+
+img {
+  display: block;
+}
+
+a {
+  color: lightblue;
+}
+
+.story-intro {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.75);
+  padding: 20px;
+  color: white;
+  /**/
+  padding-right: calc(var(--bs-gutter-x) / 2);
+  padding-left: calc(var(--bs-gutter-x) / 2);
+  margin-top: var(--bs-gutter-y);
+}
+
+h1 {
+  font-size: 4em;
+}
+h1,
+h2 {
+  margin: 0 0 10px 0;
+}
+
+.story-intro h1 {
+  font-size: 2.5em;
+}
+.story-intro p {
+  margin: 0;
+}
+
+.sub-stories {
+  overflow: hidden;
+  margin: 0 0 25px 0;
+}
+
+.sub-story {
+  float: left;
+  width: 250px;
+  margin-right: 25px;
+  position: relative;
+  font-size: 80%;
+}
+
+.last {
+  margin-right: 0;
+}
+
+/* ----------- iPhone 4 and 4S ----------- */
+
+/* Portrait and Landscape */
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Portrait */
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Landscape */
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* ----------- iPhone 5, 5S, 5C and 5SE ----------- */
+
+/* Portrait and Landscape */
+@media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Portrait */
+@media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Landscape */
+@media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* ----------- iPhone 6, 6S, 7 and 8 ----------- */
+
+/* Portrait and Landscape */
+@media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Portrait */
+@media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Landscape */
+@media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* ----------- iPhone 6+, 7+ and 8+ ----------- */
+
+/* Portrait and Landscape */
+@media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) {
+  .col-lg-4,
+  .story-intro {
+    width: 70% !important;
+    background: rgba(0, 0, 0, 0.75) !important;
+    padding: 20px !important;
+    color: white !important;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Portrait */
+@media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: portrait) {
+  .col-lg-4,
+  .story-intro {
+    width: 70% !important;
+    background: rgba(0, 0, 0, 0.75) !important;
+    padding: 20px !important;
+    color: white !important;
+  }
+  .tile-image {
+    padding-left: 50px !important;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Landscape */
+@media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+  .tile-image {
+    padding-left: 50px !important;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* ----------- iPhone X ----------- *
+
+/* Portrait */
+@media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: portrait) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+  }
+  .tile-image {
+    padding-left: 50px !important;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
+}
+
+/* Landscape */
+@media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+  .col-lg-4,
+  .story-intro {
+    width: 70%;
+    background: rgba(0, 0, 0, 0.75);
+    padding: 20px;
+    color: white;
+  }
+  .tile-image {
+    padding-left: 50px !important;
+    padding-right: calc(var(--bs-gutter-x) / 2);
+    padding-left: calc(var(--bs-gutter-x) / 2);
+    margin-top: var(--bs-gutter-y);
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,91 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        starwars-fan-page
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="page-wrap rounded-xl container">
+    <h1>Responsive Page</h1>
+
+    <p>While maintaining the heirarchy of importance.</p>
+
+    <article class="main-story">
+      <img
+        class="hero-image"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wallpaperup.com%2Fuploads%2Fwallpapers%2F2014%2F11%2F29%2F533542%2Fa37d9fa7a5a62031250fd7b75ec6c880-700.jpg&f=1&nofb=1"
+        alt="Sha Ra Rocking"
+        width="1000"
+        height="400"
+      />
+
+      <div class="story-intro">
+        <h1>Most Important Story</h1>
+        <p>
+          This article has the most visual weight.
+          <a
+            href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wallpaperup.com%2Fuploads%2Fwallpapers%2F2014%2F11%2F29%2F533542%2Fa37d9fa7a5a62031250fd7b75ec6c880-700.jpg&f=1&nofb=1"
+            >image source.</a
+          >
+        </p>
       </div>
-    </div>
+    </article>
+
+    <section class="sub-stories flex justify-center">
+      <article class="sub-story">
+        <img class="tile-image" src="http://placekitten.com/250/350" />
+        <div class="story-intro">
+          <h2>Less Important Story</h2>
+          <p>This story has less visual weight.</p>
+        </div>
+      </article>
+
+      <article class="sub-story">
+        <img
+          class="tile-image"
+          src="https://starwars-fan-page.s3.us-east-2.amazonaws.com/dog-starwars-1.jpg"
+        />
+        <div class="story-intro">
+          <h2>Less Important Story</h2>
+          <p>This story has less visual weight.</p>
+        </div>
+      </article>
+
+      <article class="sub-story last">
+        <img class="tile-image" src="http://placebear.com/250/350" />
+        <div class="story-intro">
+          <h2>Less Important Story</h2>
+          <p>This story has less visual weight.</p>
+        </div>
+      </article>
+    </section>
+    <!-- New Section -->
+    <section class="row">
+      <div class="col-lg-4">
+        <img class="tile-image" src="http://placekitten.com/250/350" />
+        <div class="story-intro">
+          <h2>Less Important Story</h2>
+          <p>This story has less visual weight.</p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <img
+          class="tile-image"
+          src="https://starwars-fan-page.s3.us-east-2.amazonaws.com/dog-starwars-1.jpg"
+        />
+        <div class="story-intro">
+          <h2>Less Important Story</h2>
+          <p>This story has less visual weight.</p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <img class="tile-image" src="http://placebear.com/250/350" />
+        <div class="story-intro">
+          <h2>Less Important Story</h2>
+          <p>This story has less visual weight.</p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
